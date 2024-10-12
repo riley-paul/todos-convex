0d786c3c77285892eaf992eefb3615d0d9bce487
Riley Paul <rileypaul96@gmail.com>
2024-10-12 09:16:43 -0600

    create adder

--- src/components/adder.tsx
+++ src/components/adder.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useAtomValue } from "jotai/react";
+import { selectedListAtom } from "@/lib/store";
+import { toast } from "sonner";
+import { Plus } from "lucide-react";
 
 const Adder: React.FC = () => {
+  const selectedList = useAtomValue(selectedListAtom);
+  const [value, setValue] = React.useState("");
+
   return (
-    <div className="flex gap-2">
-      <Input />
-      <Button>Add</Button>
-    </div>
+    <form
+      className="flex gap-2"
+      onSubmit={(e) => {
+        e.preventDefault();
+        toast.success("Task added", { description: value });
+        setValue("");
+      }}
+    >
+      <Input
+        autoFocus
+        placeholder="What needs doing?"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Button type="submit">
+        <Plus className="size-5 mr-2" />
+        <span>Add</span>
+      </Button>
+      <input type="submit" hidden />
+    </form>
   );
 };
 
