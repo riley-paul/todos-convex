e6c95f6e48196cec8c0f0944da2f5866371ff7fa
Riley Paul <rileypaul96@gmail.com>
2024-10-12 08:04:04 -0600

    add fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import { Chat } from "@/Chat/Chat";
-import { ChatIntro } from "@/Chat/ChatIntro";
-import { Layout } from "@/Layout";
 import { SignInForm } from "@/sign-in-form";
-import { UserMenu } from "@/components/UserMenu";
 import { Authenticated, Unauthenticated, useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
+import Header from "./header";
 
 export default function App() {
   const user = useQuery(api.users.viewer);
   return (
     <>
       <Authenticated>
-        <Layout menu={<UserMenu>{user?.name ?? user?.email}</UserMenu>}>
-          <ChatIntro />
-          <Chat viewer={(user ?? {})._id!} />
-        </Layout>
+        <Header />
+        <Chat viewer={(user ?? {})._id!} />
       </Authenticated>
       <Unauthenticated>
         <main className="min-h-screen flex items-center justify-center">
--- src/header.tsx
+++ src/header.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header className="h-14 bg-card/20 backdrop-blur border-b">
+    <header className="h-14 bg-card/20 backdrop-blur border-b sticky top-0 z-50">
       <div className="max-w-screen-sm mx-auto flex items-center h-full">
         <h1 className="text-2xl font-bold tracking-tight">
-          <i className="fa-solid fa-save" />
+          <i className="fa-solid fa-circle mr-2 text-primary" />
           <span>Todos</span>
         </h1>
       </div>
